99c9e1c5330d98b50887a13b9b500ec8b6b772fe
Paul Zibarov <paul.zibarov@gmail.com>
2020-08-19 15:37:50 +0300

    Add CoW metric.

---
+++ TextAnalyzer/Metrics/CountOfWords.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.SymbolStore;
+
+namespace TextAnalyzer.Metrics
+{
+    public class CountOfWords
+    {
+        public static string Analyze(string text)
+        {
+            const char dash = '-';
+            const char apostrophe = '\'';
+            
+            int position = 0;
+            int countOfWords = 0;
+
+            while (position < text.Length && (char.IsPunctuation(text[position]) || char.IsWhiteSpace(text[position])))
+            {
+                position++;
+            }
+
+            while (position < text.Length)
+            {
+                while (position < text.Length && (char.IsLetter(text[position]) || char.IsDigit(text[position]) || char.IsPunctuation(text[position])))
+                {
+                    position++;
+                }
+                
+                countOfWords++;
+                
+                while (position < text.Length && (char.IsPunctuation(text[position]) || char.IsWhiteSpace(text[position])))
+                {
+                    position++;
+                }
+            }
+
+            return $"Count of words {countOfWords}";
+        }
+    }
+}
