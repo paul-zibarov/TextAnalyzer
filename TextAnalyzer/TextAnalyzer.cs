79753d61054d80082edd242ec7d21c5b261d8af4
Paul Zibarov <paul.zibarov@gmail.com>
2020-08-19 18:19:29 +0300

    Add TextAnalyzer class.

---
+++ TextAnalyzer/TextAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using TextAnalyzer.Factories;
+using TextAnalyzer.Metrics;
+
+namespace TextAnalyzer
+{
+    public class TextAnalyzer
+    {
+        private IMetricFactory _factory;
+        
+        public TextAnalyzer(IMetricFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public string GetAnalysisResult(string text)
+        {
+            var metrics = _factory.GetAllMetrics();
+            var result = string.Empty;
+            
+            foreach (var m in metrics)
+            {
+                result = result + '\n' + m.Analyze(text) ;
+            }
+
+            return result;
+        }
+    }
+}
