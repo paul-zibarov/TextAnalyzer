cbf44738be17a11acfe3efd642cb8c00b19be077
Paul Zibarov <paul.zibarov@gmail.com>
2020-08-19 18:22:33 +0300

    Add views and model for analysis result.

---
+++ TextAnalyzer/Models/ResultViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TextAnalyzer.Models
+{
+    public class ResultViewModel
+    {
+        public string Message { get; set; }
+
+        public ResultViewModel(string result)
+        {
+            Message = result;
+        }
+    }
+}
