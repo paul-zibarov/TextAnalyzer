4ef878adaecfb03a8aa9466fccc80226a4a69350
Paul Zibarov <paul.zibarov@gmail.com>
2020-08-19 18:20:54 +0300

    Add MetricFactory and TestAnalyzerServices to ConfigureServices method.

--- TextAnalyzer/Startup.cs
+++ TextAnalyzer/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TextAnalyzer.Factories;
+using TextAnalyzer.Metrics;
 
 namespace TextAnalyzer
 {
@@ -24,6 +26,8 @@ namespace TextAnalyzer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IMetricFactory, MetricFactory>();
+            services.AddTransient<TextAnalyzer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
