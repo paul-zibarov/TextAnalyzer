5c74235929fb9b90bc97021bd97b6eae02de2f72
Paul Zibarov <paul.zibarov@gmail.com>
2020-08-19 18:18:41 +0300

    Add MetricFactory.

---
+++ TextAnalyzer/Factories/IMetricFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TextAnalyzer.Enums;
+using TextAnalyzer.Metrics;
+
+namespace TextAnalyzer.Factories
+{
+    public interface IMetricFactory
+    {
+        ITextMetric GetMetric(MetricType type);
+        ICollection<ITextMetric> GetAllMetrics();
+    }
+}
---
+++ TextAnalyzer/Factories/MetricFactory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using TextAnalyzer.Enums;
+using TextAnalyzer.Metrics;
+
+namespace TextAnalyzer.Factories
+{
+    public class MetricFactory : IMetricFactory
+    {
+        public ITextMetric GetMetric(MetricType type)
+        {
+            switch (type)
+            {
+                case MetricType.CountOfWords:
+                    return new CountOfWordsMetric();
+                case MetricType.CountOfParagraphs:
+                    return new CountOfParagraphsMetric();
+                case MetricType.CountOfPunctuationSymbols:
+                    return new CountOfPunctuationSymbolsMetric();
+                case MetricType.CountOfSymbols: 
+                    return new CountOfSymbolsMetric();
+                default: 
+                    return null;
+            }
+        }
+
+        public ICollection<ITextMetric> GetAllMetrics()
+        {
+            return new List<ITextMetric>()
+            {
+                new CountOfSymbolsMetric(),
+                new CountOfWordsMetric(),
+                new CountOfParagraphsMetric(),
+                new CountOfPunctuationSymbolsMetric()
+            };
+        }
+    }
+}
